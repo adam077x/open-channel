5d29a577ff9031089a1bf535f272cc0ec7de3c0f
adam077x <adam.heczko@gmail.com>
2022-01-26 17:29:19 +0100

    Readme and some fixes

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -4,7 +4,7 @@ const Post = require('../models/Post');
 const router = new Router();
 
 router.post("/create_post", async function(req, res) {
-    if(req.body.title.length > 0 && req.body.text.length > 0 && req.body.title.length < 64 && req.body.text.length < 255) {
+    if(req.body.title.length > 0 && req.body.text.length > 0 && req.body.title.length < 64 && req.body.text.length < 2048) {
         await Post.create(req.body);
         res.redirect(`/board/${req.body.boardId}`);    
     }
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Open Channel
+This is simple web application that allows you to create posts on anonymous website for everyone to see.
+
+![Home Page](1.png)
+![Board](2.png)
